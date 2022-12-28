5143cafc8ae9a6ffafd987b64a86cc67abf023be
Reynolds Sharp <sharpdesignco@Reynoldss-iPro.attlocal.net>
2022-12-27 22:36:51 -0600

    JS snow and card hovers working

--- main.js
+++ main.js
@@ -1,19 +1,59 @@
-// const card = document.querySelector('.card');
-// let tiltX = 0;
-// let tiltY = 0;
+var c = document.getElementById('canv');
+var context = c.getContext("2d");
+var w = c.width = window.innerWidth;
+var h = c.height = window.innerHeight;
 
-// card.addEventListener('mousemove', function(event) {
-//   const cardRect = card.getBoundingClientRect();
-//   const mouseX = event.clientX - cardRect.left - cardRect.width / 2;
-//   const mouseY = event.clientY - cardRect.top - cardRect.height / 2;
-//   tiltX = (mouseY / cardRect.height) * 20;
-//   tiltY = -(mouseX / cardRect.width) * 20;
-//   card.style.transform = `rotateX(${tiltX}deg) rotateY(${tiltY}deg)`;
-// });
+window.addEventListener('resize', function(){
+  c.width = w = window.innerWidth;
+  c.height = h = window.innerHeight;
+}, false);
 
-// card.addEventListener('mouseleave', function() {
-//   tiltX = 0;
-//   tiltY = 0;
-//   card.style.transform = 'rotate(0)';
-// });
+Snowy();
+function Snowy() {
+    var snow, arr = [];
+    var num = 600, tsc = 1, sp = 1;
+    var sc = 1.3, t = 0, mv = 20, min = 1;
+        for (var i = 0; i < num; ++i ) {
+            snow = new Flake();
+            snow.y = Math.random() * (h + 50);
+            snow.x = Math.random() * w;
+            snow.t = Math.random() * (Math.PI * 2);
+            snow.sz = (100 / (10 + (Math.random() * 100))) * sc;
+            snow.sp = (Math.pow(snow.sz * .8, 2) * .15) * sp;
+            snow.sp = snow.sp < min ? min : snow.sp;
+            arr.push(snow);
+        }
+    go();
+    function go(){
+        window.requestAnimationFrame(go);
+        context.clearRect(0, 0, w, h);
+        context.fillStyle = 'hsla(242, 95%, 3%, 1)';
+        context.fillRect(0, 0, 2, h);
+        context.fill();
+            for (var i = 0; i < arr.length; ++i) {
+                f = arr[i];
+                f.t += 0.5;
+                f.t = f.t >= Math.PI *2 ? 0 : f.t;
+                f.y += f.sp;
+                f.x += Math.sin(f.t * tsc) * (f.sz * .3);
+                if (f.y > h + 50) f.y = -10 - Math.random() * mv;
+                if (f.x > w + mv) f.x = -mv;
+                if (f.x < - mv) f.x = w + mv;
+                f.draw();
+            }
+    }
+    function Flake() {
+        this.draw = function() {
+            this.g = context.createRadialGradient(this.x, this.y, 0, this.x, this.y, this.sz);
+                this.g.addColorStop(0, 'hsla(255, 255%, 255%, 1)');
+                this.g.addColorStop(1, 'hsla(255, 255%, 255%, 0)');
+                context.moveTo(this.x, this.y);
+                context.fillStyle = this.g;
+                context.beginPath();
+                context.arc(this.x, this.y, this.sz, 0, Math.PI * 2, true);
+                context.fill();
+            }
+        }
+    }
+    
 
